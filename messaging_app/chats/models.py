aae233c229b925b7c758b0ce3617457a4b6a7510
Erick Ochieng <erickochieng830@gmail.com>
2025-06-05 18:15:30 +0300

    messaging

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -12,7 +12,6 @@ class User(AbstractUser):
     """
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=128)  # Added password field
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
