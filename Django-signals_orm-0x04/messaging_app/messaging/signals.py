ba014bd7c82a316097110af7d5159bc090896704
Erick Ochieng <erickochieng830@gmail.com>
2025-06-13 17:42:31 +0300

    signals

--- Django-signals_orm-0x04/messaging_app/messaging/signals.py
+++ Django-signals_orm-0x04/messaging_app/messaging/signals.py
@@ -54,13 +54,31 @@ def cleanup_user_data(sender, instance, **kwargs):
     Signal to clean up user-related data when a user is deleted.
     Task 2: Automatically clean up related data when a user deletes their account.
     """
-    # Note: This signal will be triggered when a User is deleted
-    # Django's CASCADE will handle most of the cleanup automatically
-    # but we can add custom logic here if needed
+    # Clean up messages sent by the deleted user
+    sent_messages = Message.objects.filter(sender=instance)
+    sent_messages.delete()
     
-    # Custom cleanup logic can be added here
-    # For example, anonymizing messages instead of deleting them
-    pass
+    # Clean up messages received by the deleted user
+    received_messages = Message.objects.filter(receiver=instance)
+    received_messages.delete()
+    
+    # Clean up notifications for the deleted user
+    user_notifications = Notification.objects.filter(user=instance)
+    user_notifications.delete()
+
+
+@receiver(post_save, sender=Message)
+def cleanup_old_notifications(sender, instance, created, **kwargs):
+    """
+    Signal to clean up old notifications to prevent notification overflow.
+    """
+    if created:
+        # Keep only the latest 50 notifications per user
+        user_notifications = Notification.objects.filter(user=instance.receiver).order_by('-created_at')
+        if user_notifications.count() > 50:
+            old_notifications = user_notifications[50:]
+            notification_ids = [notif.id for notif in old_notifications]
+            Notification.objects.filter(id__in=notification_ids).delete()
 
 
 # Additional utility functions for signal management
@@ -69,6 +87,7 @@ def disconnect_signals():
     post_save.disconnect(create_message_notification, sender=Message)
     pre_save.disconnect(log_message_edit, sender=Message)
     post_delete.disconnect(cleanup_user_data, sender=User)
+    post_save.disconnect(cleanup_old_notifications, sender=Message)
 
 
 def reconnect_signals():
@@ -76,3 +95,4 @@ def reconnect_signals():
     post_save.connect(create_message_notification, sender=Message)
     pre_save.connect(log_message_edit, sender=Message)
     post_delete.connect(cleanup_user_data, sender=User)
+    post_save.connect(cleanup_old_notifications, sender=Message)
